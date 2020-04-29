065f1eb4db1d40272d1a7267025ad8faee9939c8
Jules Prévost <jprevostdev@gmail.com>
2020-04-29 12:20:35 +0200

    add examples of arithmetic and methods used in Ruby

--- first_program.rb
+++ first_program.rb
@@ -1,1 +1,45 @@
+=begin
+The puts method
+=end
+
 puts "Hello world"
+puts "I'm alive and well!"
+
+puts
+
+puts "J'ai 5 euros, soit 10% de ma fortune personnelle, je suis riche!"
+
+puts 
+
+puts 5
+puts 5 + 3
+
+puts "Hello"
+puts "world"
+
+puts "Hi there,
+this is a line break"
+
+=begin
+The print method, no add os line break
+=end
+
+print "Hello"
+print "world"
+puts
+
+print "Hello" + " " + "world"
+puts 
+print "Hello"
+print " "
+print "world"
+puts
+
+=begin
+The p method
+=end
+
+p "string"
+p "Hi there,
+this is a line break"
+
